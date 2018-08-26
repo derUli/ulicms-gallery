f95c7e5430c66a98ccfe55e012aff22882eb8a34
Ulrich Schmidt <support@ulicms.de>
2018-08-26 21:41:57 +0200

    lightbox in extra modul ausgelagert

--- src/content/modules/gallery2019/controllers/Gallery2019Controller.php
+++ src/content/modules/gallery2019/controllers/Gallery2019Controller.php
@@ -7,27 +7,6 @@ class Gallery2019Controller extends Controller
 
     public const MODULE_NAME = "gallery2019";
 
-    public function head()
-    {
-        $cssFile = ModuleHelper::buildRessourcePath(self::MODULE_NAME, "js/lightbox2/css/lightbox.min.css");
-        echo Style::FromExternalFile($cssFile);
-    }
-
-    public function frontendFooter()
-    {
-        $translation = new JSTranslation(array(), "LightBoxTranslation");
-        $translation->addKey("image_x_of_y");
-        $translation->render();
-        
-        $jsFile = ModuleHelper::buildRessourcePath(self::MODULE_NAME, "js/lightbox2/js/lightbox.min.js");
-        $frontendJs = ModuleHelper::buildRessourcePath(self::MODULE_NAME, "js/frontend.js");
-        
-        enqueueScriptFile($jsFile);
-        enqueueScriptFile($frontendJs);
-        
-        combinedScriptHtml();
-    }
-
     public function uninstall()
     {
         $migrator = new DBMigrator("module/{self::MODULE_NAME}", ModuleHelper::buildModuleRessourcePath(self::MODULE_NAME, "sql/down"));
--- src/content/modules/gallery2019/lang/de.php
+++ src/content/modules/gallery2019/lang/de.php
@@ -5,4 +5,3 @@ add_translation("edit_gallery", "Galerie bearbeiten");
 add_translation("image_amount", "Anzahl der Bilder");
 add_translation("add_image", "Bild hinzufügen");
 add_translation("edit_image", "Bild bearbeiten");
-add_translation("image_x_of_y", "Bild %1 von %2");
